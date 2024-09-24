307c6dd3562073c4a20faffedbc00e82de3192ba
Mumit Mohammad <mamumit07@gmail.com>
2024-09-24 01:14:54 +0100

    Add html table to show ranking

--- src/league.test.ts
+++ src/league.test.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { parseLeagueResults } from "./league";
 
 test('parse current result', () => {
-  const results = parseLeagueResults("./public/leagues")
+  const results = parseLeagueResults()
   assert(results.length >= 1, `Leagues found ${results.length}`)
   results.forEach(result => {
     const numPlayers = Object.keys(result.players).length;
